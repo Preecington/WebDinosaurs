0cad21de6b2189c4e7466896579ad84e0e41f001
Ryan Preece <ryan.preece@outlook.com>
2018-09-08 17:20:30 +1000

    Implemented basic Webpack

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+var $ = require('jquery');
+var moment = require('moment');
+
 $(document).ready(function() {
     var currDate = moment().format('dddd');
     $("#sp").append("<br />The current day is: " + currDate);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+module.exports = {
+    mode: 'development',
+    entry: './scripts.js',
+    output: {
+        filename: 'main.js',
+        publicPath: 'dist'
+    }
+};
