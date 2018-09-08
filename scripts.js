db47282401076d8c66d9e86e51f958a490aed6cb
Ryan Preece <ryan.preece@outlook.com>
2018-09-08 19:18:30 +1000

    Basic React App

--- scripts.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import moment from 'moment';
-
-$(document).ready(function() {
-    var currDate = moment().format('dddd');
-    $("#sp").append(`<br />The current day is: ${currDate}.`);
-});
-
