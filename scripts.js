3323455740afea971ef37e893ca43aaddba59744
Ryan Preece <ryan.preece@outlook.com>
2018-09-08 19:04:40 +1000

    Implemented task runners

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ import moment from 'moment';
 
 $(document).ready(function() {
     var currDate = moment().format('dddd');
-    $("#sp").append(`<br />The current day is: ${currDate}`);
+    $("#sp").append(`<br />The current day is: ${currDate}.`);
 });
 
